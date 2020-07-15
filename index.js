8707436346c9b57cc3767c0beb7fc84aac33a874
Graham <grahamfalconer@icloud.com>
2020-07-15 15:03:42 +0100

    graham sq io

--- index.js
+++ index.js
@@ -4,27 +4,35 @@ var context = canvas.getContext('2d');
 var xPos = 0;
 var yPos = 0;
 
-context.rect(xPos, yPos, 50, 50);
+context.rect(xPos, yPos, 150, 150);
 context.stroke();
 
 function move(e){
   console.log(e.keyCode);
   //left
   if(e.keyCode==37){
-
+    xPos-=50;
   }
+
   //right
   if(e.keyCode==39){
-
+    xPos+=50;
   }
+
   //up
   if(e.keyCode==38){
-
+    yPos-=50;
   }
+
   //down
   if(e.keyCode==40){
-
+    yPos+=50;
   }
+
+  canvas.width=canvas.width;
+  context.rect(xPos, yPos, 150, 150);
+  context.stroke();
+
 
 }
 
